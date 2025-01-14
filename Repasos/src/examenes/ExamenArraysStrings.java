32ddc1722e2121bcbd31569b9d7d1f294875632d
Fg-21 <fm.gonzalez@iesnervion.es>
2025-01-14 17:32:39 +0100

    compruebaCódigo

--- Repasos/src/examenes/ExamenArraysStrings.java
+++ Repasos/src/examenes/ExamenArraysStrings.java
@@ -1,6 +1,7 @@
 package examenes;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class ExamenArraysStrings {
 //Abecedario ordenado	
@@ -12,99 +13,147 @@ public class ExamenArraysStrings {
 			"las guerras seguiran mientras el color de la piel siga siendo mas importante que el de los ojos",
 			"aprende a vivir y sabras morir bien", "cada dia sabemos mas y entendemos menos",
 			"el dinero no puede comprar la vida", "la verdadera sabiduria esta en reconocer la propia ignorancia" };
-	
-	
-	//Frase con la que se juega
+
+	// Frase con la que se juega
 	static String fraseReal;
-	
-	//Frase codificada
+
+	// Frase codificada
 	static String fraseCodificada;
-	
-	//Randomizador
+
+	// Randomizador
 	static Random rand = new Random();
 
+	// Scanner
+	static Scanner rd = new Scanner(System.in);
+
 	public static void main(String[] args) {
+
+		// Variable para la letra del usuario
+		String letra;
+
+		// Variable para la comprobación del código
+		boolean check = false;
 		
-		//Desordenamos el array del abecedario
+		// Variable para la comprobación de victoria
+		boolean win = false;
+		
+		//Código de una letra
+		
+
+		// Desordenamos el array del abecedario
 		desordenaAbecedario();
-		
-		//Elegimos una frase random
-		fraseReal=eligeFrase();
-		
-		//Codificamos la frase
+
+		// Elegimos una frase random
+		fraseReal = eligeFrase();
+
+		// Codificamos la frase
 		fraseCodificada = codificaFrase();
+
+		// Pedimos al usuario que adivine una letra del códico hasta que lo adivine
+		while (win) {
+			System.out.println(fraseCodificada);
+			System.out.println("Di una letra para adivinar la frase");
+			letra = rd.next();
+
+			// Comprobamos si la letra está en la frase
+			check = compruebaCódigo(code, letra);
+
+			// Si está, le decimos que ha acertado, si no, le decimos que ha fallado
+			if (check) {
+				System.out.println("Acertaste!");
+			} else {
+				System.out.println("Has fallado!");
+			}
+			
+			//Comprobamos si el jugador ha ganado
+			win = sonIguales();
+		}
 		
-		System.out.println(fraseCodificada);
+		//Damos la enhorabuena al jugador
+		System.out.println("¡HAS GANADO, ENHORABUENA!");
 	}
-	
-	static void desordenaAbecedario(){
-		//Variable auxiliar para guardar la letra a cambiar
+
+	static void desordenaAbecedario() {
+		// Variable auxiliar para guardar la letra a cambiar
 		char letra;
-		
-		//Variable para el número aleatorio de las columnas
+
+		// Variable para el número aleatorio de las columnas
 		int numRC;
-		
-		//Variable para el número aleatorio de las filas
+
+		// Variable para el número aleatorio de las filas
 		int numRF;
-		//Recorremos el bucle desordenando las letras
+		// Recorremos el bucle desordenando las letras
 		for (int i = 0; i < abecedario.length; i++) {
 			for (int j = 0; j < abecedario[i].length; j++) {
-				//Creo una posición aleatoria
+				// Creo una posición aleatoria
 				numRC = rand.nextInt(0, abecedario[0].length);
 				numRF = rand.nextInt(0, abecedario.length);
-				
-				//Guardo la letra aleatoria
+
+				// Guardo la letra aleatoria
 				letra = abecedario[numRF][numRC];
-				
-			
-				
-				//La sustituyo en la posición que toque
+
+				// La sustituyo en la posición que toque
 				abecedario[numRF][numRC] = abecedario[i][j];
 				abecedario[i][j] = letra;
 			}
 		}
 	}
-	
+
 	static String eligeFrase() {
-		//Número aleatorio para desordenar la frase
+		// Número aleatorio para desordenar la frase
 		int numR;
-		
-		//Variable para la frase aleatoria
+
+		// Variable para la frase aleatoria
 		String frase;
-		
-		//Randomizamos
+
+		// Randomizamos
 		numR = rand.nextInt(0, frases.length);
-		
-		//Asignamos la frase aleatoria
+
+		// Asignamos la frase aleatoria
 		frase = frases[numR];
-		
-		return frase;	
+
+		return frase;
 	}
-	
+
 	static String codificaFrase() {
-		//Frase codificada
+		// Frase codificada
 		String fraseCoded = "";
-		
-		//Contador para la busqueda
+
+		// Contador para la busqueda
 		int cont = 0;
-		
-		//Recorremos la frase sustituyendo la letra por la posición en el abecedario
+
+		// Indicador para la busqueda
+		boolean finded;
+
+		// Recorremos la frase sustituyendo la letra por la posición en el abecedario
 		for (int i = 0; i < fraseReal.length(); i++) {
-			for (int j = 0; j < abecedario.length; j++) {
-				while (abecedario[j][cont] != fraseReal.charAt(i)) {
+			finded = false;
+			for (int j = 0; j < abecedario.length && !finded; j++) {
+				cont = 0;
+				while (cont < abecedario[j].length && !finded) {
 					if (fraseReal.charAt(i) == abecedario[j][cont]) {
-						fraseCoded += j + " ";
-						fraseCoded += cont + " ";
+						fraseCoded += j;
+						fraseCoded += cont;
+
+						finded = true;
 					}
+
 					cont++;
 				}
-				cont = 0;
 			}
-			
+			fraseCoded += " ";
 		}
-		
-		
-		
+
 		return fraseCoded;
+	}
+
+	// Metodo comprobar código
+	static boolean compruebaCódigo(String letra) {
+		// Booleano a retornar
+		boolean flag;
+		
+		
+		
+		return flag;
 	}
 }
