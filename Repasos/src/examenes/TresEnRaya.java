c8952342c101f3a055dbcd9a4173564c7b568284
Fg-21 <fm.gonzalez@iesnervion.es>
2025-01-15 18:19:17 +0100

    tres en raya mal

--- Repasos/src/examenes/TresEnRaya.java
+++ Repasos/src/examenes/TresEnRaya.java
@@ -17,7 +17,7 @@ public class TresEnRaya {
 		int turno;
 
 		// Booleano para ver si el tablero está lleno
-		boolean lleno;
+		boolean lleno = false;
 
 		// posicion filas usuario
 		int posiFU = 0;
@@ -25,14 +25,20 @@ public class TresEnRaya {
 		// posición columna usuario
 		int posiCU = 0;
 
-		// caracter que para la victoria
-		char caracter;
+		// condicion ganadora
+		boolean win = false;
+		
+		//jugar mas
+		char playMore;
+		
+		//limpiar tablero
+		boolean limpiar = false;
 
 		// Scanner
 		Scanner sc = new Scanner(System.in);
 
 		// se sigue jugando hasta que uno de los 2 gane o se complete el tablero
-		while (limpiaTablero()) {
+		while (!limpiar) {
 			// Decidmos quien empieza a jugar
 			turno = jugadorInicial();
 
@@ -44,7 +50,7 @@ public class TresEnRaya {
 			// damos la bienvenida
 			System.out.println("¡BIENVENIDOS AL TRES EN RAYA!");
 
-			while (!esGanador(caracter) || !lleno) {
+			while (!win || !lleno) {
 				// Si el turno es 1 juega la maquina, si es 0 juega el jugador
 				if (turno == 1) {
 					System.out.println("Turno de la máquina");
@@ -56,7 +62,39 @@ public class TresEnRaya {
 					imprimeTablero();
 
 					// Comprobamos si alguien ha ganado
-					esGanador(caracter);
+					win = esGanador('X');
+					
+					//decimos quien gana
+					if (win) {
+						System.out.println("GANO LA MÁQUINA");
+					}
+					
+					System.out.println("Tu turno!");
+
+					do {
+						// imprimimos el tablero
+						imprimeTablero();
+
+						// preguntamos al usuario la fila y la columna que quiere modificar
+						System.out.println("Fila");
+						posiFU = sc.nextInt();
+
+						System.out.println("Columna");
+						posiCU = sc.nextInt();
+
+						// usuario mueve ficha
+						usuarioMueveFicha(posiFU, posiCU);
+
+					} while (!usuarioMueveFicha(posiFU, posiCU));
+
+					// Comprobamos si alguien ha ganado
+					win = esGanador('O');
+					
+					//decimos quien gana
+					if (win) {
+						System.out.println("GANASTEEE");
+					}
+					
 
 				} else {
 					System.out.println("Tu turno!");
@@ -75,11 +113,31 @@ public class TresEnRaya {
 						// usuario mueve ficha
 						usuarioMueveFicha(posiFU, posiCU);
 
-					} while (usuarioMueveFicha(posiFU, posiCU));
+					} while (!usuarioMueveFicha(posiFU, posiCU));
 
 					// Comprobamos si alguien ha ganado
-					esGanador(caracter);
+					win = esGanador('O');
+					
+					//decimos quien gana
+					if (win) {
+						System.out.println("GANASTEEE");
+					}
+					
+					System.out.println("Turno de la máquina");
 
+					// la maquina pone ficha
+					mueveFichaAleatoria();
+
+					// imprimimos el tablero
+					imprimeTablero();
+
+					// Comprobamos si alguien ha ganado
+					win = esGanador('X');
+					
+					//decimos quien gana
+					if (win) {
+						System.out.println("GANO LA MÁQUINA");
+					}
 				}
 
 				// Comprobamos si el tablero está relleno recorriendolo
@@ -92,8 +150,24 @@ public class TresEnRaya {
 				}
 
 			}
+			//Preguntamos si quiere jugar más
+			System.out.println("Quieres jugar otra vez?: y=Sí, n=NO");
+			playMore = sc.next().charAt(0);
+			
+			switch (playMore) {
+			case 'y' ->{
+				limpiar = limpiaTablero(playMore);
+				System.out.println("ADIOOOOS");
+			}
+			case 'n' ->{
+				System.out.println("ADIOOOOOS");
+			}
+			
+			}
 		}
 
+		//Cierro scanner
+		sc.close();
 	}
 
 	// Imprime el tablero
@@ -156,7 +230,7 @@ public class TresEnRaya {
 		boolean valid = false;
 
 		// vemos si no hay nada en medio para poner la ficha
-		if (tablero[posiFU][posiCU] == '-') {
+		if (tablero[posiFU][posiCU] != '-' || posiFU < tablero.length || posiCU < tablero[0].length) {
 			// ponemos en el tablero un círculo en las coordenadas especificadas
 			tablero[posiFU][posiCU] = 'O';
 			valid = true;
@@ -170,7 +244,55 @@ public class TresEnRaya {
 		// variable para ver si has ganado o no
 		boolean win = false;
 
+		// recorremos el tablero comprobando si hay 3 caracteres seguidos iguales
+		for (int i = 0; i < tablero.length; i++) {
+		    for (int j = 0; j < tablero[0].length; j++) {
+
+		        // Comprobar "tres en raya" horizontal
+		        if (j <= tablero[0].length - 3) { // Asegúrate de que hay espacio para verificar hacia la derecha
+		            if (tablero[i][j] == caracter && tablero[i][j + 1] == caracter && tablero[i][j + 2] == caracter) {
+		                win = true;
+		            }
+		        }
+
+		        // Comprobar "tres en raya" vertical
+		        if (i <= tablero.length - 3) { // Asegúrate de que hay espacio para verificar hacia abajo
+		            if (tablero[i][j] == caracter && tablero[i + 1][j] == caracter && tablero[i + 2][j] == caracter) {
+		                win = true;
+		            }
+		        }
+
+		        // Comprobar "tres en raya" en la diagonal principal (↘)
+		        if (i <= tablero.length - 3 && j <= tablero[0].length - 3) { // Asegúrate de que hay espacio hacia la diagonal ↘
+		            if (tablero[i][j] == caracter && tablero[i + 1][j + 1] == caracter && tablero[i + 2][j + 2] == caracter) {
+		                win = true;
+		            }
+		        }
+
+		        // Comprobar "tres en raya" en la diagonal secundaria (↙)
+		        if (i >= 2 && j <= tablero[0].length - 3) { // Asegúrate de que hay espacio hacia la diagonal ↙
+		            if (tablero[i][j] == caracter && tablero[i - 1][j + 1] == caracter && tablero[i - 2][j + 2] == caracter) {
+		                win = true;
+		            }
+		        }
+		    }
+		}
+
 		// devolvemos la posible victoria
 		return win;
+	}
+	
+	static boolean limpiaTablero(char decision) {
+		boolean limpia = true;
+		
+		if (decision == 'y') {
+			for (int i = 0; i < tablero.length; i++) {
+				Arrays.fill(tablero[i], '-');
+			}
+			limpia = false;
+		}
+		
+		
+		return limpia;
 	}
 }
